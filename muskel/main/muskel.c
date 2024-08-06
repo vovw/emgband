4bcc62e7f3262e26fc979f688b0b5f07d5674fdf
atharva <voidz7447@gmail.com>
2024-08-06 17:02:50 +0530

    added the thing that works finally

--- muskel/main/muskel.c
+++ muskel/main/muskel.c
@@ -1,260 +1,109 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-#include "esp_spiffs.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
 #include "driver/adc.h"
-#include "esp_timer.h"
+#include "esp_adc_cal.h"
 
-#define CONSTRAIN_EMG_LOW 0
-#define CONSTRAIN_EMG_HIGH 1000
-#define SAMPLE_RATE 300
-#define FFT_SIZE 1280 // Must be a power of 2
-#define BUFFER_SIZE 128
-#define MAGNITUDE_THRESHOLD 0.01
+#define SAMPLE_RATE 500
+#define INPUT_PIN ADC1_CHANNEL_6
+#define BUFFER_SIZE 64
 
-// ANSI color codes
-#define ANSI_COLOR_GREEN "\x1b[32m"
-#define ANSI_COLOR_BLUE "\x1b[34m"
-#define ANSI_COLOR_ORANGE "\x1b[33m"
-#define ANSI_COLOR_RED "\x1b[31m"
-#define ANSI_COLOR_RESET "\x1b[0m"
+static int circular_buffer[BUFFER_SIZE];
+static int data_index = 0, sum = 0;
 
-typedef struct
-{
-    float real;
-    float imag;
-} complex_t;
+static esp_adc_cal_characteristics_t adc_chars;
 
-float bound(float value, float low, float high);
-float map(float x, float in_min, float in_max, float out_min, float out_max);
-float bandpass_filter_75_150(float input);
-void fft(complex_t *x, int n);
-void bit_reverse(complex_t *x, int n);
-void apply_hanning_window(complex_t *x, int n);
-void print_colored_magnitude();
-void print_beauty(float magnitude);
+static int getEnvelope(int abs_emg);
+static float EMGFilter(float input);
+void emg(void);
+void plotASCII(int value, int min, int max, int width);
 
-void emg();
+const TickType_t xDelay = 1000 / SAMPLE_RATE / portTICK_PERIOD_MS;
 
-// ADC channels for the 3 sensors
-#define INPUT_PIN1 ADC1_CHANNEL_6 // pin 34
-#define INPUT_PIN2 ADC1_CHANNEL_7 // pin 35
-#define INPUT_PIN3 ADC1_CHANNEL_0 // pin 36
-
-int circular_buffer[BUFFER_SIZE];
-int data_index = 0, sum = 0;
-
-static complex_t samples1[FFT_SIZE];
-static complex_t samples2[FFT_SIZE];
-static complex_t samples3[FFT_SIZE];
-static int sample_index = 0;
 
 void app_main(void)
 {
     xTaskCreate(emg, "does emg things", 4096, NULL, tskIDLE_PRIORITY, NULL);
 }
 
-static esp_timer_handle_t timer;
-static bool timer_expired = false;
-
-void timer_callback(void* arg)
+void emg(void)
 {
-    timer_expired = true;
-}
-
-void emg()
-{
-    // Configure ADC width and channel attenuation for all three channels
     adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(INPUT_PIN1, ADC_ATTEN_DB_11);
-    adc1_config_channel_atten(INPUT_PIN2, ADC_ATTEN_DB_11);
-    adc1_config_channel_atten(INPUT_PIN3, ADC_ATTEN_DB_11);
-    printf("START\n");
+    adc1_config_channel_atten(INPUT_PIN, ADC_ATTEN_DB_11);
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 0, &adc_chars);
 
-    // Create and start the timer
-    esp_timer_create_args_t timer_args = {
-        .callback = &timer_callback,
-        .name = "one_second_timer"
-    };
-    ESP_ERROR_CHECK(esp_timer_create(&timer_args, &timer));
-    ESP_ERROR_CHECK(esp_timer_start_once(timer, 1000000)); // 1 second in microseconds
+    while (1) {
+	vTaskDelay(xDelay);
 
-    while (!timer_expired)
-    {
-        if (sample_index < FFT_SIZE)
-        {
-            // Read sensor values
-            float sensor_value1 = (float)adc1_get_raw(INPUT_PIN1);
-            float sensor_value2 = (float)adc1_get_raw(INPUT_PIN2);
-            float sensor_value3 = (float)adc1_get_raw(INPUT_PIN3);
+        int adc_reading = adc1_get_raw(INPUT_PIN);
+        int voltage = esp_adc_cal_raw_to_voltage(adc_reading, &adc_chars);
 
-            // ... (keep the rest of the processing code)
+	// TODO : check and confront this
+        // Convert voltage to original Arduino's 0-1023 scale
+        int sensor_value = (voltage * 1023) / 4095;
 
-            sample_index++;
-        }
-        else
-        {
-            // Print the magnitude of each sample
-            print_colored_magnitude();
+        // Filtered EMG
+        int signal = (int)EMGFilter((float)sensor_value);
 
-            sample_index = 0; // Reset sample index for the next set of samples
-        }
+        printf("%d\n", signal);
     }
-
-    // Clean up and exit
-    esp_timer_delete(timer);
-    printf("Stopping after 1 second\n");
-    vTaskDelete(NULL);
 }
 
-
-
-void print_colored_magnitude()
+int getEnvelope(int abs_emg)
 {
-    for (int i = 0; i < FFT_SIZE / 2; i++)
-    {
-        float magnitude1 = sqrt(samples1[i].real * samples1[i].real + samples1[i].imag * samples1[i].imag);
-        float magnitude2 = sqrt(samples2[i].real * samples2[i].real + samples2[i].imag * samples2[i].imag);
-        float magnitude3 = sqrt(samples3[i].real * samples3[i].real + samples3[i].imag * samples3[i].imag);
-
-        printf("%.2f,%.2f,%.2f\n", magnitude1, magnitude2, magnitude3);
-    }
-    printf("\n"); // End of line for the next set of data
-}
-void print_beauty(float magnitude)
-{
-    const char *color;
-    if (magnitude < 5)
-    {
-        color = ANSI_COLOR_GREEN;
-    }
-    else if (magnitude < 10)
-    {
-        color = ANSI_COLOR_BLUE;
-    }
-    else if (magnitude < 20)
-    {
-        color = ANSI_COLOR_ORANGE;
-    }
-    else
-    {
-        color = ANSI_COLOR_RED;
-    }
-    printf("%.2f,", magnitude);
-    // printf("%s %.2f%s\n", color, magnitude, ANSI_COLOR_RESET);
-}
-float bound(float value, float low, float high)
-{
-    if (value < low)
-        return low;
-    if (value > high)
-        return high;
-    return value;
+    sum -= circular_buffer[data_index];
+    sum += abs_emg;
+    circular_buffer[data_index] = abs_emg;
+    data_index = (data_index + 1) % BUFFER_SIZE;
+    return (sum/BUFFER_SIZE) * 2;
 }
 
-float map(float x, float in_min, float in_max, float out_min, float out_max)
-{
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-// Band-Pass Butterworth IIR digital filter, generated using filter_gen.py.
-// Sampling rate: 500.0 Hz, frequency: [75.0, 150.0] Hz.
-// Filter is order 4, implemented as second-order sections (biquads).
-// Reference: https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.butter.html
-float bandpass_filter_75_150(float input)
+float EMGFilter(float input)
 {
     float output = input;
     {
         static float z1, z2; // filter section state
-        float x = output - -1.64891342 * z1 - 0.77932149 * z2;
-        output = 0.00014412 * x + 0.00028824 * z1 + 0.00014412 * z2;
+        float x = output - 0.05159732*z1 - 0.36347401*z2;
+        output = 0.01856301*x + 0.03712602*z1 + 0.01856301*z2;
         z2 = z1;
         z1 = x;
     }
     {
         static float z1, z2; // filter section state
-        float x = output - -1.75188517 * z1 - 0.82786889 * z2;
-        output = 1.00000000 * x + 2.00000000 * z1 + 1.00000000 * z2;
+        float x = output - -0.53945795*z1 - 0.39764934*z2;
+        output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
         z2 = z1;
         z1 = x;
     }
     {
         static float z1, z2; // filter section state
-        float x = output - -1.69281282 * z1 - 0.88994696 * z2;
-        output = 1.00000000 * x + -2.00000000 * z1 + 1.00000000 * z2;
+        float x = output - 0.47319594*z1 - 0.70744137*z2;
+        output = 1.00000000*x + 2.00000000*z1 + 1.00000000*z2;
         z2 = z1;
         z1 = x;
     }
     {
         static float z1, z2; // filter section state
-        float x = output - -1.88326655 * z1 - 0.93936089 * z2;
-        output = 1.00000000 * x + -2.00000000 * z1 + 1.00000000 * z2;
+        float x = output - -1.00211112*z1 - 0.74520226*z2;
+        output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
         z2 = z1;
         z1 = x;
     }
     return output;
 }
 
-void apply_hanning_window(complex_t *x, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        float w = 0.5 * (1 - cos(2 * M_PI * i / (n - 1)));
-        x[i].real *= w;
-        x[i].imag *= w;
-    }
-}
-
-void fft(complex_t *x, int n)
-{
-    bit_reverse(x, n);
-
-    for (int s = 1; s <= log2(n); s++)
-    {
-        int m = 1 << s; // 2 power s
-        float omega = -2.0 * M_PI / m;
-
-        for (int k = 0; k < n; k += m)
-        {
-            for (int j = 0; j < m / 2; j++)
-            {
-                complex_t t;
-                float omega_t = j * omega;
-                t.real = cos(omega_t);
-                t.imag = sin(omega_t);
-
-                complex_t u = x[k + j];
-                complex_t v;
-                v.real = t.real * x[k + j + m / 2].real - t.imag * x[k + j + m / 2].imag;
-                v.imag = t.real * x[k + j + m / 2].imag + t.imag * x[k + j + m / 2].real;
-
-                x[k + j].real = u.real + v.real;
-                x[k + j].imag = u.imag + v.imag;
-
-                x[k + j + m / 2].real = u.real - v.real;
-                x[k + j + m / 2].imag = u.imag - v.imag;
-            }
+void plotASCII(int value, int min, int max, int width) {
+    int range = max - min;
+    int position = (value - min) * width / range;
+    
+    for (int i = 0; i < width; i++) {
+        if (i == position) {
+            printf("*");
+        } else {
+            printf("-");
         }
     }
+    printf(" %d\n", value);
 }
 
-void bit_reverse(complex_t *x, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        int j = 0;
-        for (int k = 0; k < log2(n); k++)
-        {
-            j = (j << 1) | ((i >> k) & 1);
-        }
-        if (j > i)
-        {
-            complex_t temp = x[i];
-            x[i] = x[j];
-            x[j] = temp;
-        }
-    }
-}
